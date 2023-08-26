1e341b4b027776f833059b1257b214b9e40da9d9
NathanB98 <97767277+NathanB98@users.noreply.github.com>
2023-08-26 20:23:46 +0100

    Function for adding a book to library. Takes input, creates an Object and pushes this to the myLibrary array.

--- src/script.js
+++ src/script.js
@@ -1,4 +1,17 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        "author": "nathan",
+        "title": "the book",
+        "pages": 250,
+        "read": false
+    },
+    {
+        "author": "tolkein",
+        "title": "fellowship",
+        "pages": 460,
+        "read": true
+    }
+];
 
 // Book Object constructor //
 function Book(author, title, pages, read) {
@@ -8,6 +21,7 @@ function Book(author, title, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
-    // Do stuff here
+function addBookToLibrary(author, title, pages, read) {
+    const userAddedBook = new Book(author, title, pages, read);
+    myLibrary.push(userAddedBook);
 }
