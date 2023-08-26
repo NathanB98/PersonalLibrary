7f923541dc5e941c0ff2397ac0aec79b71acca62
NathanB98 <97767277+NathanB98@users.noreply.github.com>
2023-08-26 20:10:53 +0100

    Book constructor implemented.

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+// Book Object constructor //
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    // Do stuff here
+}
